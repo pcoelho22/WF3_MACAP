bee004373fe0f6118577f996f6a3052179beab1e
Garfield9110 <prfabri@yahoo.fr>
2016-07-09 12:15:47 +0200

    mise en place de la validation du formulaire d'inscription, pas oublier de mettre le champ use_password en bd en varchar 60 ou +

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -4,45 +4,38 @@ namespace Controller;
 
 use \W\Controller\Controller;
 use \W\Security\AuthentificationManager;
-use \W\Manager\UserManager;
-use \Manager\UserManagerr;
+use \Manager\UserManager;
 
-class UserController extends Controller
-{
+class UserController extends Controller {
 
     /**
      * Page d'accueil par défaut
      */
-    public function login(){
+    public function login() {
         $this->show('default/login');
     }
 
-    public function loginVal(){
+    public function loginVal() {
         $usernameOrEmail = isset($_POST['usernameOrEmail']) ? trim(strip_tags($_POST['usernameOrEmail'])) : '';
         $password = isset($_POST['password']) ? trim(strip_tags($_POST['password'])) : '';
 
         $authManager = new AuthentificationManager();
         $usr_id = $authManager->isValidLoginInfo($usernameOrEmail, $password);
 
-        $userManager = new UserManager();
-        $foundUser = $userManager->getUserByUsernameOrEmail($usernameOrEmail);
-
-        if($usr_id === 0){
-            print_r($foundUser) ;
+        if ($usr_id === 0) {
             echo 'Arf :: login invalide <br/>';
-        }
-        else{
+        } else {
             $userManager = new UserManager();
             $authManager->logUserIn($userManager->find($usr_id));
             $this->redirectToRoute('home');
         }
     }
 
-    public function signUp(){
+    public function signUp() {
         $this->show('default/signUp');
     }
 
-    public function signupVal(){
+    public function signupVal() {
         $username = isset($_POST['userName']) ? trim(strip_tags($_POST['userName'])) : '';
         $lastName = isset($_POST['lastName']) ? trim(strip_tags($_POST['lastName'])) : '';
         $firstName = isset($_POST['firstName']) ? trim(strip_tags($_POST['firstName'])) : '';
@@ -55,18 +48,47 @@ class UserController extends Controller
         $passwordVerif = isset($_POST['passwordVerif']) ? trim(strip_tags($_POST['passwordVerif'])) : '';
 
         // Il manque la validation des données
-
-        if ($password != '' && $password == $passwordVerif) {
-            $userManager = new UserManagerr();
-            if ($userManager->insert(['use_userName'=>$username, 'use_name'=>$lastName, 'use_firstName'=>$firstName, 'use_adress'=>$adress, 'use_postCode'=>$zip, 'use_phone'=>$phone, 'use_fax'=>$fax, 'use_email'=>$email, 'use_password'=>password_hash($password, PASSWORD_BCRYPT), 'use_role'=>'user', 'use_dateCreation'=>time()])){
-                $this->redirectToRoute('user_login');
+        if (strlen($username) >= 5) {
+            if ($lastName != '') {
+                if ($firstName != '') {
+                    if (strlen($adress) >= 10) {
+                        if (strlen($zip) >= 4) {
+                            if (strlen($phone) >= 5) {
+                                if (strlen($fax) >= 5) {
+                                    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                                        if ($password != '' && $password == $passwordVerif) {
+                                            $userManager = new UserManager();
+                                            if ($userManager->insert(['use_userName' => $username, 'use_name' => $lastName, 'use_firstName' => $firstName, 'use_adress' => $adress, 'use_postCode' => $zip, 'use_phone' => $phone, 'use_fax' => $fax, 'use_email' => $email, 'use_password' => password_hash($password, PASSWORD_BCRYPT), 'use_role' => 'user', 'use_dateCreation' => time()])) {
+                                                $this->redirectToRoute('user_login');
+                                            }
+                                        } else {
+                                            echo 'Arf :: password vide!<br />';
+                                            exit;
+                                        }
+                                    } else {
+                                        echo 'veuillez entrer un email correcte';
+                                    }
+                                } else {
+                                    echo 'veuillez entrer numero de fax valide';
+                                }
+                            } else {
+                                echo 'veuillez indiquez votre numero de telephone';
+                            }
+                        } else {
+                            echo "veuillez indiquez votre code postal";
+                        }
+                    } else {
+                        echo "veuillez entrer votre adresse";
+                    }
+                } else {
+                    echo 'veuillez indiquez votre Prenom';
+                }
+            } else {
+                echo 'veuillez indiquez votre Nom';
             }
+        } else {
+            echo 'username pas assez long';
         }
-        else {
-            echo 'Arf :: password vide!<br />';
-            exit;
-        }
-
     }
 
 }
--- app/routes.php
+++ app/routes.php
@@ -2,9 +2,9 @@
 
 $w_routes = array(
     ['GET', '/', 'Default#home', 'home'],
-    ["GET|POST", "contact/", "Default#contact", "default_contact"],
-    ["GET", "login/", "User#login", "user_login"],
-    ["POST", "login/", "User#loginVal", "user_loginval"],
-    ["GET", "login/signup", "User#signUp", "user_signup"],
-    ["POST", "login/signup", "User#signUpVal", "user_signupval"],
+    ["GET|POST", "/contact/", "Default#contact", "default_contact"],
+    ["GET", "/login/", "User#login", "user_login"],
+    ["POST", "/login/", "User#loginVal", "user_loginval"],
+    ["GET", "/login/signup", "User#signUp", "user_signup"],
+    ["POST", "/login/signup", "User#signUpVal", "user_signupval"],
 );
