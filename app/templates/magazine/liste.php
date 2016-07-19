34320dd87a627a49e4c1f3353d34e354f6ca4d87
pcoelho22 <webdev@psgroup.lu>
2016-07-19 14:03:37 +0200

    Modification taille image magazine

--- app/templates/magazine/liste.php
+++ app/templates/magazine/liste.php
@@ -4,7 +4,7 @@
 	<h2>Liste des magazines disponible</h2>
 	<ul><br/>
 		<?php foreach ($magazineListe as $key => $value) : ?>
-			<li><a href="<?= $this->assetUrl($value['mag_path'])?>"><?= $value['mag_name'].'<br/> '.$value['mag_date']?><img src="<?= $this->assetUrl($value['mag_couverture'])?>"></a></li><br/>
+			<li><a href="<?= $this->assetUrl($value['mag_path'])?>"><?= $value['mag_name'].'<br/> '.$value['mag_date']?><img width="300px" height="400px" src="<?= $this->assetUrl($value['mag_couverture'])?>"></a></li><br/>
 		<?php endforeach ?>
 	</ul>
 	
