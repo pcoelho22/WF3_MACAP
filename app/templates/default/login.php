1e900e42bf82b2203f2abcedb8209f110fc30daa
triggerbite <craimundo75@gmail.com>
2016-07-20 15:32:12 +0200

    layout login page

--- app/templates/default/login.php
+++ app/templates/default/login.php
@@ -1,7 +1,8 @@
 <?php $this->layout('layout', ['title' => 'Login !']) ?>
 
 <?php $this->start('main_content') ?>
-<h2>Se connecter</h2>
+
+<!--<h2>Se connecter</h2>
 
 <form action="" method="post">
     <label for="usernameOrEmail">Username Or Email</label><br/>
@@ -12,12 +13,41 @@
 
     <input type="submit" value="Login">
 </form>
+-->
+
+<h1>Login</h1>
+
+<form action="#" method="POST" role="form">
+		<div class="input-group">
+			<span class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i></span>
+			<input type="text" name="usernameOrEmail" value="" required="" placeholder="username/email" class="form-control text-left">
+		</div>
+
+		<span class="help-block"></span>
+
+		<div class="input-group">
+			<span class="input-group-addon"><i class="fa fa-unlock" aria-hidden="true"></i></span>
+			<input type="password" name="password" value="" required="" placeholder="password" class="form-control text-left">
+		</div>
+		<span class="help-block"></span>
+		<button class="btn btn-primary btn-block btn-md" type="submit" value="Login">Login</button>									
+		<div>
+			<span class="help-block"></span>
+			<p>
+			<a href="/WF3_MACAP/public/login/forgot"><button class="btn btn-default btn-md">Forgot password?</button></a>
+			<a href="/WF3_MACAP/public/login/signup"><button class="btn btn-default btn-md">Register</button></a>
+			</p>
+		</div>
+	</form>
+
 <?php if (isset($erreur)){
     echo $erreur;
 } ?>
 
+<!--
 <br/>
 <a href="<?= $this->url('user_signup') ?>"><button>Se crée un compte</button></a>
 <a href="<?= $this->url('user_forgot') ?>"><button>Mot de passe oublier</button></a>
+-->
 
 <?php $this->stop('main_content') ?>
