d65b7ec9b31456966ee2c32224be2c01c4a570b0
Etudiant <Etudiant>
2016-07-21 14:15:36 +0200

    changement
    titre

--- app/templates/galerie/photos.php
+++ app/templates/galerie/photos.php
@@ -1,9 +1,8 @@
 <?php $this->layout('layout', ['title' => 'Photos !']) ?>
 
 <?php $this->start('main_content') ?>
-	<h2>Photos de chaque galerie</h2>
-	<h2>Galerie description</h2>
-	<?= $eventsIdGaleires['gal_description'] ?>
+	<h2>Photos de <?= $eventsIdGaleries['gal_name'] ?></h2>
+	<?= $eventsIdGaleries['gal_description'] ?>
 	<ul><br/>
 		<?php foreach ($photosGalerie as $key => $value) : ?>
 			<li><?= $value['pho_legend'].'<br/> '.$value['pho_name']?><br/><img src="<?= $this->assetUrl($value['pho_path'])?>"></li><br/>
