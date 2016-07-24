b1397f853f7eb2348593f1a2949e40ab4fa88e82
triggerbite <craimundo75@gmail.com>
2016-07-24 19:59:44 +0200

    integration formulaire

--- app/templates/default/edit.php
+++ app/templates/default/edit.php
@@ -1,85 +1,129 @@
-<?php $this->layout('layout', ['title' => 'Modifié les informations de votre compte']) ?>
+<?php $this->layout('layout', ['title' => 'Modifier les informations de votre compte']) ?>
 
 <?php $this->start('main_content') ?>
-<h2>Modifié les informations de votre compte</h2>
-<?php
-if (isset($error)) {
-    debug($error);
-}
-debug($_POST);
-?>
+<h2>Modifier les informations de votre compte</h2>
+<div class="row">
 
+    <div class="col-md-7 text-left">
 <?php if (isset($vals)): ?>
-    <form action="" method="post">
-        <label for="name">Last Name</label><br/>
-        <input id="name" type="text" name="lastName" value="<?= $vals['use_name'] ?>" placeholder="Ex: Reuter"><br/><br/>
+        <form action="" method="post">
+            <span class="help-block"></span>
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-user fa-fw" aria-hidden="true"></i></span>
+                <input id="name" type="text" name="lastName" value="<?= $vals['use_name'] ?>" required="" placeholder="Nom" class="form-control text-left">
+            </div>
+            <span class="help-block"></span>
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-user fa-fw" aria-hidden="true"></i></span>
+                <input id="firstName" type="text" name="firstName" value="<?= $vals['use_first_name'] ?>" placeholder="Prénom" class="form-control text-left">
+            <span class="help-block"></span>
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-road fa-fw" aria-hidden="true"></i></span>
+                <input id="adress" type="text" name="adress" value="<?= $vals['use_address'] ?>" placeholder="N° et rue" class="form-control text-left">
+            </div>
+            <span class="help-block"></span>
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-tags fa-fw" aria-hidden="true"></i></span>
+                <input id="postCode" type="text" name="postCode" value="<?= $vals['use_post_code'] ?>" placeholder="Code postal" class="form-control text-left">
+            </div>
+            <span class="help-block"></span>
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-home fa-fw" aria-hidden="true"></i></span>
+                <input id="city" type="text" name="city" value="<?= $vals['use_city'] ?>" placeholder="Ville" class="form-control text-left">
+            </div>
+            <span class="help-block"></span>
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-phone fa-fw" aria-hidden="true"></i></span>
+                <input id="phone" type="text" name="phone" value="<?= $vals['use_phone'] ?>" placeholder="N° de téléphone (Format: (00 + Code Pays) + votre numéro) Ex: 00352-11 11 11 11" class="form-control text-left">
+            </div>
+            <span class="help-block"></span>
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-fax fa-fw" aria-hidden="true"></i></span>
+                <input id="fax" type="text" name="fax" value="<?= $vals['use_fax'] ?>" placeholder="N° de fax (Optionel - Format: (00 + Code Pays) + votre numéro) Ex: 00352-11 11 11 11" class="form-control text-left">
+            </div>
+            <span class="help-block"></span>
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-lock fa-fw" aria-hidden="true"></i></span>
+                <input id="passwordVerif" type="password" name="passwordVerif" value="<?= $vals['use_password'] ?>" placeholder="Enter Password (minumum 6 charactères avec au moins 1 majuscule et 1 chiffre)" class="form-control text-left">
+            </div>
+            <span class="help-block"></span>
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-lock fa-fw" aria-hidden="true"></i></span>
+                <input id="passwordVerif" type="password" name="passwordVerif" value="" placeholder="Re-enter Password (minumum 6 charactères avec au moins 1 majuscule et 1 chiffre)" class="form-control text-left">
+            </div>
+            <span class="help-block"></span>
+            <button class="btn btn-primary btn-sm active" type="submit" value="Valider les modifications" href="#"><i class="fa fa-pencil fa-fw"></i> Valider les modifications</button>
+            <span class="help-block"></span>
+            <h4>Une fois les données modifiées, vous serez redirigé vers la page d'accueil</h4>
+        </form>
+    </div>
+    <span class="help-block"></span>
+    <?php if (isset($error)): ?>
+        <div class="col-md-5 text-left">
+            <div class="alert alert-danger fade in" rows="auto">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                <strong>Erreur!</strong><br>
+                <ul>
+                    <?php foreach ($error as $value): ?>
+                        <li><?= $value ?></li>
+                    <?php endforeach; ?>    
+                </ul>
+            </div>
+        </div> 
+    <?php endif; ?>
 
-        <label for="firstName">First Name</label><br/>
-        <input id="firstName" type="text" name="firstName" value="<?= $vals['use_first_name'] ?>" placeholder="Ex: Paul"><br/><br/>
+    <?php elseif (isset($values)): ?>
 
-        <label for="adress">Adress</label><br/>
-        <input id="adress" type="text" name="adress" value="<?= $vals['use_address'] ?>" placeholder="Ex:9, Rue des Hauts Founeaux, Belval"><br/><br/>
-
-        <label for="city">City</label><br/>
-        <input id="city" type="text" name="city" value="<?= $vals['use_city'] ?>" placeholder="Ex: Belval"><br/><br/>
-
-        <label for="postCode">Code Postal</label><br/>
-        <input id="postCode" type="text" name="postCode" value="<?= $vals['use_post_code'] ?>" placeholder="Ex: 4362 Esch-sur-Alzette"><br/><br/>
-
-        <label for="phone">Numéro de Téléphone (Format: (00 + Code Pays)-Votre numero)</label><br/>
-        <label for="phone">Exemple: 00352-11 11 11 11</label><br/>
-        <input id="phone" type="text" name="phone" value="<?= $vals['use_phone'] ?>" placeholder="Ex: 00352-11 11 11 11"><br/><br/>
-
-        <label for="fax">Numéro de Fax (Optionel - Format: (00 + Code Pays)-Votre numero)</label><br/>
-        <label for="fax">Exemple: 00352-11 11 11 11</label><br/>
-        <input id="fax" type="text" name="fax" value="<?= $vals['use_fax'] ?>" placeholder="Ex: 00352-11 11 11 11"><br/><br/>
-
-        <label for="password">Password (minumum 6 charactères avec au moins 1 majuscule et 1 chiffre)</label><br/>
-        <input id="password" type="password" name="password" value="<?= $vals['use_password'] ?>" ><br/><br/>
-
-        <label for="passwordVerif">Re-enter Password</label><br/>
-        <input id="passwordVerif" type="password" name="passwordVerif" value="" ><br/><br/>
-
-        <br>
-
-        <input type="submit" value="Valider les modifications">
-    </form>
-<?php elseif (isset($values)): ?>
-    <?php debug($values); ?>
-    <form action="" method="post">
-
-        <label for="name">Last Name</label><br/>
-        <input id="name" type="text" name="lastName" value="<?= $values['use_name'] ?>" placeholder="Ex: Reuter"><br/><br/>
-
-        <label for="firstName">First Name</label><br/>
-        <input id="firstName" type="text" name="firstName" value="<?= $values['use_first_name'] ?>" placeholder="Ex: Paul"><br/><br/>
-
-        <label for="adress">Adress</label><br/>
-        <input id="adress" type="text" name="adress" value="<?= $values['use_address'] ?>" placeholder="Ex:9, Rue des Hauts Founeaux"><br/><br/>
-
-        <label for="city">City</label><br/>
-        <input id="city" type="text" name="city" value="<?= $values['use_city'] ?>" placeholder="Belval"><br/><br/>
-
-        <label for="postCode">Code Postal</label><br/>
-        <input id="postCode" type="text" name="postCode" value="<?= $values['use_post_code'] ?>" placeholder="Ex: 4362 Esch-sur-Alzette"><br/><br/>
-
-        <label for="phone">Numéro de Téléphone (Format: (00 + Code Pays)-Votre numero)</label><br/>
-        <label for="phone">Exemple: 00352-11 11 11 11</label><br/>
-        <input id="phone" type="text" name="phone" value="<?= $values['use_phone'] ?>" placeholder="Ex: 00352-11 11 11 11"><br/><br/>
-
-        <label for="fax">Numéro de Fax (Optionel - Format: (00 + Code Pays)-Votre numero)</label><br/>
-        <label for="fax">Exemple: 00352-11 11 11 11</label><br/>
-        <input id="fax" type="text" name="fax" value="<?= $values['use_fax'] ?>" placeholder="Ex: 00352-11 11 11 11"><br/><br/>
-
-        <label for="password">Password (minumum 6 charactères avec au moins 1 majuscule et 1 chiffre)</label><br/>
-        <input id="password" type="password" name="password" value="" ><br/><br/>
-
-        <label for="passwordVerif">Re-enter Password</label><br/>
-        <input id="passwordVerif" type="password" name="passwordVerif" value="" ><br/><br/>
-
-        <br>
-        <input type="submit" value="Valider les modifications">
+    <form action="#" method="post" role="form">
+        <div class="input-group">
+            <span class="input-group-addon"><i class="fa fa-user fa-fw" aria-hidden="true"></i></span>
+            <input id="name" type="text" name="lastName" value="<?= $values['use_name'] ?>" required="" placeholder="Nom de la personne en charge" class="form-control text-left">
+        </div>
+        <span class="help-block"></span>
+        <div class="input-group">
+            <span class="input-group-addon"><i class="fa fa-user fa-fw" aria-hidden="true"></i></span>
+            <input id="firstName" type="text" name="firstName" value="<?= $values['use_first_name'] ?>" placeholder="Prénom" class="form-control text-left">
+        </div>
+        <span class="help-block"></span>
+        <div class="input-group">
+            <span class="input-group-addon"><i class="fa fa-road fa-fw" aria-hidden="true"></i></span>
+            <input id="adress" type="text" name="adress" value="<?= $values['use_address'] ?>" placeholder="N° et rue" class="form-control text-left">
+        </div>
+        <span class="help-block"></span>
+        <div class="input-group">
+            <span class="input-group-addon"><i class="fa fa-tags fa-fw" aria-hidden="true"></i></span>
+            <input id="postCode" type="text" name="postCode" value="<?= $values['use_post_code'] ?>" placeholder="Code postal" class="form-control text-left">
+        </div>
+         <span class="help-block"></span>
+        <div class="input-group">
+            <span class="input-group-addon"><i class="fa fa-home fa-fw" aria-hidden="true"></i></span>
+            <input id="city" type="text" name="city" value="<?= $values['use_city'] ?>" placeholder="Ville" class="form-control text-left">
+        </div>
+        <span class="help-block"></span>
+        <div class="input-group">
+            <span class="input-group-addon"><i class="fa fa-phone fa-fw" aria-hidden="true"></i></span>
+            <input id="phone" type="text" name="phone" value="<?= $values['use_phone'] ?>" placeholder="N° de téléphone (Format: (00 + Code Pays) + votre numéro) Ex: 00352-11 11 11 11" class="form-control text-left">
+        </div>
+        <span class="help-block"></span>
+        <div class="input-group">
+            <span class="input-group-addon"><i class="fa fa-fax fa-fw" aria-hidden="true"></i></span>
+            <input id="fax" type="text" name="fax" value="<?= $values['use_fax'] ?>" placeholder="N° de fax (Optionel - Format: (00 + Code Pays) + votre numéro) Ex: 00352-11 11 11 11" class="form-control text-left">
+        </div>
+        <span class="help-block"></span>
+        <div class="input-group">
+            <span class="input-group-addon"><i class="fa fa-lock fa-fw" aria-hidden="true"></i></span>
+            <input id="password" type="password" name="password" value="" placeholder="Enter Password (minumum 6 charactères avec au moins 1 majuscule et 1 chiffre)" class="form-control text-left" class="ng-scope ng-pristine ng-valid-pattern ng-valid ng-valid-required">
+        </div>
+        <span class="help-block"></span>
+        <div class="input-group">
+            <span class="input-group-addon"><i class="fa fa-lock fa-fw" aria-hidden="true"></i></span>
+            <input id="passwordVerif" type="password" name="passwordVerif" value="" placeholder="Re-enter Password (minumum 6 charactères avec au moins 1 majuscule et 1 chiffre)" class="form-control text-left">
+        </div>
+         <span class="help-block"></span>
+        <button class="btn btn-primary btn-sm active" type="submit" value="Valider les modifications" href="#"><i class="fa fa-pencil fa-fw"></i> Valider les modifications</button>
+        <span class="help-block"></span>
+        <h4>Une fois les données modifiées, vous serez redirigé vers la page d'accueil</h4>
     </form>
 <?php endif; ?>
-
+</div>
 <?php $this->stop('main_content') ?>
