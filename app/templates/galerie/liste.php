bedbef22ea4b2ff737af7e47a9c1ce290bd7252f
raven095 <raven90_94@hotmail.com>
2016-07-25 16:53:55 +0200

    Deplacement bouton ajout sur la liste des galleries

--- app/templates/galerie/liste.php
+++ app/templates/galerie/liste.php
@@ -4,11 +4,13 @@
 <h2>Liste de galeries</h2>
 <div class="row">
     <div class="col-md-12 text-left">
+        <ul class="list-group">
+            <li class="list-group-item">
+                <a class="btn btn-default btn-sm" href="<?= $this->url('galerie_add') ?>">Ajouter une reportages</a>
+            </li>
+        </ul>
         <?php foreach ($galerieListe as $key => $value) : ?>
             <ul class="list-group">
-                <li class="list-group-item">
-                    <a class="btn btn-default btn-sm" href="<?= $this->url('galerie_add') ?>">Ajouter une reportages</a>
-                </li>
                 <li class="list-group-item">
                     <a href="<?= $this->url('galerie_photos', ['id' => $value['id']]) ?>"><?= $value['gal_name'] . '<br/> ' . $value['gal_legend'] ?></a>
                 </li>
