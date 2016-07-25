b880be7defaa53c60c70036bc269fb9ad4141d64
raven095 <raven90_94@hotmail.com>
2016-07-25 10:06:41 +0200

    Màj liste news

--- app/templates/news/liste.php
+++ app/templates/news/liste.php
@@ -26,31 +26,4 @@
 			<?php endforeach ?>
 		</div>
 	</div>
-<h2>Liste des news</h2>
-<div class="row">
-    <div class="col-md-12 text-left">
-        <?php foreach ($newsListe as $key => $value) : ?>
-            <ul class="list-group">
-                <li class="list-group-item">
-                    <a class="btn btn-default btn-sm" href="<?= $this->url('news_add') ?>">Ajouter une news</a>
-                </li>
-                <li  class="list-group-item">
-                    <img alt="" width="120" heigth="120" src="<?= $value['con_avatar'] ?>">
-                    <div class="detailsTitleevents"><?= $value['con_title'] ?></div>
-                </li>
-                <li class="list-group-item">
-                    <a href="<?= $this->url('news_newsDetails', ['id' => $value['id']]) ?>"><?= $value['con_type'] . '<br/> ' . $value['con_title'] . '<br/> ' . $value['con_synopsis'] ?></a>
-                </li>
-                <?php if (isset($_SESSION['user']['use_role_opt1'])): ?>
-                    <?php if ($_SESSION['user']['use_role_opt1'] === '2'): ?>
-                        <li class="list-group-item">
-                            <a class="btn btn-default btn-sm" href="<?= $this->url('news_update', ['id' => $value['id']]) ?>">Modifier</a>
-                            <a class="btn btn-default btn-sm" href="<?= $this->url('news_deleteConfirmation', ['id' => $value['id']]) ?>">Delete</a>
-                        </li>
-                    <?php endif; ?>
-                <?php endif; ?>
-            </ul>
-        <?php endforeach ?>
-    </div>
-</div>
 <?php $this->stop('main_content') ?>
