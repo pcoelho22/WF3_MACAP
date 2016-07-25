f409a5a2861b5b6c920bc803d44978c7fb997818
triggerbite <craimundo75@gmail.com>
2016-07-25 12:32:06 +0200

    modifs layout

--- app/templates/layout Gallerie.php
+++ app/templates/layout Gallerie.php
@@ -141,7 +141,6 @@
                 <div class='row'>
                     <div class="col-12 text-center">
                         <ul class="list-inline no-margin-bottom small">
-                            <li><a href="/contact us" class="small" target="_blank">Contact Us</a></li>
                             <li><a href="/terms-conditions" class="small" target="self">Terms &amp; Conditions</a></li>
                             <li><a href="/en/sitemap" class="small" target="self">Site Map</a></li>
                         </ul>               
