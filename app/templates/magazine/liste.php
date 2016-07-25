a1bc5d5b8de49f89b6d2f00100f12e4fc467a003
Garfield9110 <prfabri@yahoo.fr>
2016-07-25 12:11:20 +0200

    add/edit/delete de magazine ajouter uniquement pour admin uniquement

--- app/templates/magazine/liste.php
+++ app/templates/magazine/liste.php
@@ -3,13 +3,19 @@
 <h2>Liste des magazines disponible</h2>
 <div class="row">
     <div class="col-md-12 text-left">
+        <ul class="list-group">
+            <?php if (isset($_SESSION['user']['use_role_opt1'])): ?>
+                <?php if ($_SESSION['user']['use_role_opt1'] === '2'): ?>
+                    <li class="list-group-item">
+                        <a class="btn btn-default btn-sm" href="<?= $this->url('magazine_add') ?>">Ajouter un magazine</a>
+                    </li>
+                <?php endif; ?>
+            <?php endif; ?>
+        </ul>
         <?php foreach ($magazineListe as $key => $value) : ?>
             <ul class="list-group">
                 <li class="list-group-item">
-                    <a class="btn btn-default btn-sm" href="<?= $this->url('magazine_add') ?>">Ajouter un magazine</a>
-                </li>
-                <li class="list-group-item">
-                    <a href="<?= $this->assetUrl($value['mag_path']) ?>"><img width="300" height="400" alt="" src="<?= $this->assetUrl($value['mag_couverture']) ?>"></a>
+                    <a href="<?= $this->assetUrl($value['mag_path']) ?>" target="_blank"><img width="300" height="400" alt="" src="<?= $this->assetUrl($value['mag_couverture']) ?>"></a>
                 </li>
                 <li  class="list-group-item">
                     <a href="<?= $this->assetUrl($value['mag_path']) ?>"><?= $value['mag_name'] . '<br/> ' . $value['mag_date'] ?></a>
