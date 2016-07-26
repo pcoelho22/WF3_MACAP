d971982d3002a5379207af0096744cb9054eacbe
triggerbite <craimundo75@gmail.com>
2016-07-26 21:13:33 +0200

    adaptation taille image

--- app/templates/default/shop.php
+++ app/templates/default/shop.php
@@ -3,7 +3,7 @@
 <?php $this->start('main_content') ?>
 <div class="row">
 	<div class="col-sm-12"></div>
-		<img src="<?= $this->assetUrl('img/page-underConstruction.png') ?>" class="center-block img-responsive" alt="image" >
+		<img src="<?= $this->assetUrl('img/page-underConstruction.png') ?>" class="center-block img-responsive" alt="image" style="width: 950px;">
 	</div>
 </div>
 
