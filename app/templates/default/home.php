2ac18441bd276fdb5cbf3b5408629d33e6b37848
triggerbite <craimundo75@gmail.com>
2016-07-26 10:34:21 +0200

    ajout PHP Slider

--- app/templates/default/home.php
+++ app/templates/default/home.php
@@ -2,30 +2,34 @@
 
 <?php $this->start('main_content') ?>
 <div class="row">
-    <div id="carousel-example-generic" class="col-lg-12 carousel slide" data-ride="carousel">
+    <div id="myCarousel" class="col-sm-12 carousel slide img-responsive" data-ride="carousel">
         <!-- Wrapper for slides -->
         <div class="carousel-inner" role="listbox">
-            <div class="item active">
-                <img src="<?= $this->assetUrl('img/Slider/pic4.jpg') ?>" class="img-responsive  img-rounded" alt="">
+            <div class="item active img-responsive">
+                <img src="<?= $this->assetUrl('img/Slider/pic4.jpg') ?>" class="img-responsive img-rounded" alt="">
                 <div class="carousel-caption">
                     <h3>Retrocar</h3>
                     <p>Classic cars making history</p>
                 </div>
             </div>
-            <div class="item">
-                <img src="<?= $this->assetUrl("img/Slider/") ?>" class="img-responsive  img-rounded" alt="">
+            <?php foreach ($imageSlider as $key => $photo):
+            ?>
+            <div class="item img-responsive">
+                <img src="<?= $this->assetUrl($photo['pho_path']) ?>" class="img-responsive img-rounded" alt="">
                 <div class="carousel-caption">
                     <h3>Retrocar</h3>
                     <p>Classic cars making history</p>
                 </div>
             </div>
+            <?php endforeach;
+            ?>
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span>
+        </a>
         </div>
 
         <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span>
-        </a>
-        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span>
-        </a>
     </div>
 </div>
 <br>
