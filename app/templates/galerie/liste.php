c3efd17448c32bb945aaf7d5c4918f9aa98c05a2
Etudiant <Etudiant>
2016-07-27 09:36:24 +0200

    changements
    affiche le button ajoute de galleries seullement pour la admin.
    creation de controler e manager pour avoir la 1 path de photo de chaque
    galerie

--- app/templates/galerie/liste.php
+++ app/templates/galerie/liste.php
@@ -4,26 +4,39 @@
 <h2>Liste de galeries</h2>
 <div class="row">
     <div class="col-md-12 text-left">
+     <?php if (isset($_SESSION['user']['use_role_opt1'])): ?>
+        <?php if ($_SESSION['user']['use_role_opt1'] === '2'): ?>
         <ul class="list-group">
             <li class="list-group-item">
-                <a class="btn btn-primary btn-sm" href="<?= $this->url('galerie_add') ?>"><span class="fa fa-pencil-square-o"></span> Ajouter une galerie</a>
+                <a href="<?= $this->url('galerie_add') ?>"><button class="btn btn-primary btn-sm"><span class="fa fa-pencil-square-o"></span>Ajouter une galerie</button></a>
             </li>
         </ul>
-        <?php foreach ($galerieListe as $key => $value) : ?>
-            <ul class="list-group">
-                <li class="list-group-item">
-                    <a href="<?= $this->url('galerie_photos', ['id' => $value['id']]) ?>"><?= $value['gal_name'] . '<br/> ' . $value['gal_legend'] ?></a>
-                </li>
-                <?php if (isset($_SESSION['user']['use_role_opt1'])): ?>
-                    <?php if ($_SESSION['user']['use_role_opt1'] === '2'): ?>
-                        <li class="list-group-item">
-                            <a class="btn btn-primary btn-sm" href="<?= $this->url('galerie_update', ['id' => $value['id']]) ?>"><span class="fa fa-pencil fa-fw"></span> Editer</a>
-                            <a class="btn btn-danger btn-sm" href="<?= $this->url('galerie_deleteConfirmation', ['id' => $value['id']]) ?>"><span class="fa fa-trash-o fa-fw"></span> Supprimer</a>
-                        </li>
-                    <?php endif; ?>
-                <?php endif; ?>
-            </ul>
-        <?php endforeach ?>
     </div>
-</div>  
+</div>
+<?php endif; ?>
+<?php endif; ?>
+<?php foreach ($galerieListe as $key => $value) : ?>
+<ul class="list-group">
+    <li class="list-group-item">
+        <a href="<?= $this->url('galerie_photos', ['id' => $value['id']]) ?>">
+            <div class="row">
+                <img class="col-md-2 text-left" width="120px" heigth="120px" src="<?= $this->assetUrl($value['pho_path'])?>">
+                <div class="col-md-10 text-left">
+                    <h3><?= $value['gal_name']?></h3>
+                    <p><?=$value['gal_legend'] ?></p>
+                </div>
+            </div>
+        </a>
+    </li>
+    <?php if (isset($_SESSION['user']['use_role_opt1'])): ?>
+        <?php if ($_SESSION['user']['use_role_opt1'] === '2'): ?>
+            <li class="list-group-item">
+                <a href="<?= $this->url('galerie_update', ['id' => $value['id']]) ?>"><button class="btn btn-primary btn-sm"><span class="fa fa-pencil fa-fw"></span> Editer</button></a>
+                <a href="<?= $this->url('galerie_deleteConfirmation', ['id' => $value['id']]) ?>"><button class="btn btn-danger btn-sm"><span class="fa fa-trash-o fa-fw"></span> Supprimer</button></a>
+            </li>
+        <?php endif; ?>
+    <?php endif; ?>
+</ul>
+<?php endforeach ?>
+
 <?php $this->stop('main_content') ?>
